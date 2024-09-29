f347dded5aca2a3eb5f74bb0729a1d9d2eefbc1e
Philip Yu <jialeyu@gmail.com>
2024-09-28 21:43:38 -0400

    convert docx to html

--- script/convertDocx.ts
+++ script/convertDocx.ts
@@ -1,2 +1,33 @@
-import mmamoth from 'mammoth'
+import * as mammoth from 'mammoth';
+import * as path from 'path';
+import * as fs from 'node:fs/promises'
 
+const fileNames = [
+  '14-Q-A-test',
+  '14-Q-B-test',
+  '14-Q-C-test',
+  'PQ1',
+  'PQ2',
+  'PQ3',
+  'PQ4',
+  'PQ5',
+];
+
+const DOCX = 'docx' as const
+const HTML = 'html' as const
+
+function createPath(name: string, ext: string) {
+  return path.join(__dirname, `./${ext}/${name}.${ext}`)
+}
+
+(async () => {
+  fileNames.forEach(async (name) => {
+    try {
+      const docxPath = createPath(name, DOCX)
+      const response = await mammoth.convertToHtml({ path: docxPath })
+      await fs.writeFile(createPath(name, HTML), response.value)
+    } catch (e) {
+      console.error('docxPaths: ' + name, e)
+    }
+  })
+})()
