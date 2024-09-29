221632f3e7330a6a2c0004905aa325f2fa685092
Philip Yu <jialeyu@gmail.com>
2024-09-29 01:09:09 -0400

    get single sheet

--- script/google-sheet.ts
+++ script/google-sheet.ts
@@ -2,6 +2,7 @@ import * as sheet from '@googleapis/sheets'
 import * as fs from 'node:fs/promises'
 import * as dotenv from 'dotenv'
 import * as path from 'path'
+import * as qs from 'qs'
 
 dotenv.config()
 
@@ -9,8 +10,15 @@ const SPREADSHEET_ID = process.env.SPREADSHEET_ID
 const SHEET_ID = process.env.SHEET_ID
 const API_KEY = process.env.GOOGLE_API_KEY
 
-function createSheetURL(spreadsheetId: string) {
-  return `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}?key=${API_KEY}`
+function createSheetsURL(spreadsheetId: string, apiKey: string) {
+  return `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}?key=${apiKey}`
+}
+
+const SHEET_RANGE_COLUMNS = 'A1:G30'
+
+function createSheetIdBatchURL(spreadsheetId: string, apiKey: string, ranges: string[]) {
+  const qsRanges = qs.stringify({ ranges: ranges[0] }, { indices: false, encode: false })
+  return `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${ranges[0]}?key=${apiKey}`
 }
 
 type Sheets = {
@@ -31,7 +39,7 @@ async function retrieveSheetInfo() {
       console.error('API_KEY', API_KEY)
       throw new Error('Bad process.env')
     }
-    const response = await fetch(createSheetURL(SPREADSHEET_ID))
+    const response = await fetch(createSheetsURL(SPREADSHEET_ID, API_KEY))
     const parsed = await response.json()
     await fs.writeFile(SHEET_INFO_PATH, JSON.stringify(parsed))
     return parsed satisfies Sheets
@@ -59,5 +67,32 @@ async function readLocalSheetInfo() {
     data = await retrieveSheetInfo()
   }
 
-  console.log('data', data)
+  const sheetTitles = data.sheets
+    .map(sheet => sheet.properties.title
+      && `${encodeURI(sheet.properties.title)}`)
+    .filter(name => typeof name === 'string')
+    .map(title => `${title}!${SHEET_RANGE_COLUMNS}`)
+
+  // TODO: refactor this part to be a little cleaner
+  if (typeof SPREADSHEET_ID !== 'string'
+    || typeof SHEET_ID !== 'string'
+    || typeof API_KEY !== 'string'
+  ) {
+    console.error('SPREADSHEET_ID', SPREADSHEET_ID)
+    console.error('SHEET_ID', SHEET_ID)
+    console.error('API_KEY', API_KEY)
+    throw new Error('Bad process.env')
+  }
+  
+  const sheetRangesURL = createSheetIdBatchURL(SPREADSHEET_ID, API_KEY, sheetTitles)
+
+  try {
+    const response = await fetch(sheetRangesURL)
+    const parsed = await response.json()
+
+    console.log('response', response)
+    console.log('parsed', parsed)
+  } catch (e) {
+    console.error('error fetching sheet ranges: ', e)
+  }
 })()
